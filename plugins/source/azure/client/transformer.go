11653573a45ff32cc7389ad393bc6ecc80243477
Cezary Marczak <cezary.marczak@codilime.com>
2023-03-09 09:21:36 +0100

    Add 'type' column in Subscription table

--- plugins/source/azure/client/transformer.go
+++ plugins/source/azure/client/transformer.go
@@ -1,8 +1,11 @@
 package client
 
 import (
+	"context"
 	"reflect"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/subscription/armsubscription"
+	"github.com/cloudquery/plugin-sdk/schema"
 	"github.com/cloudquery/plugin-sdk/transformers"
 )
 
@@ -13,4 +16,22 @@ func ETagNameTransformer(fld reflect.StructField) (string, error) {
 		return "etag", nil
 	}
 	return transformers.DefaultNameTransformer(fld)
+}
+
+func WithColumnValueTransformer(column, value string) schema.Transform {
+	return func(table *schema.Table) error {
+		err := transformers.TransformWithStruct(&armsubscription.Subscription{}, transformers.WithPrimaryKeys("ID"))(table)
+		if err != nil {
+			return err
+		}
+		column := schema.Column{
+			Name: column,
+			Type: schema.TypeString,
+			Resolver: func(_ context.Context, _ schema.ClientMeta, r *schema.Resource, c schema.Column) error {
+				return r.Set(c.Name, value)
+			},
+		}
+		table.Columns = append(table.Columns, column)
+		return nil
+	}
 }
--- plugins/source/azure/resources/services/subscription/subscriptions.go
+++ plugins/source/azure/resources/services/subscription/subscriptions.go
@@ -1,10 +1,8 @@
 package subscription
 
 import (
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/subscription/armsubscription"
 	"github.com/cloudquery/cloudquery/plugins/source/azure/client"
 	"github.com/cloudquery/plugin-sdk/schema"
-	"github.com/cloudquery/plugin-sdk/transformers"
 )
 
 func Subscriptions() *schema.Table {
@@ -13,7 +11,7 @@ func Subscriptions() *schema.Table {
 		Resolver:    fetchSubscriptions,
 		Description: "https://learn.microsoft.com/en-us/rest/api/resources/subscriptions/list?tabs=HTTP#subscription",
 		Multiplex:   client.SingleSubscriptionMultiplex,
-		Transform:   transformers.TransformWithStruct(&armsubscription.Subscription{}, transformers.WithPrimaryKeys("ID")),
+		Transform:   client.WithColumnValueTransformer("type", "Microsoft.Subscriptions"),
 		Columns:     schema.ColumnList{},
 		Relations: []*schema.Table{
 			locations(),
